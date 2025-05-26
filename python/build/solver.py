aacf36607f3e454c892c4265abf9a034a5ff66b6
fastbodin <aslobodin@proton.me>
2025-05-26 08:45:19 -0700

    renamed variables in python solver and cleaned up comments

--- python/build/solver.py
+++ python/build/solver.py
@@ -2,71 +2,33 @@ import numpy as np
 from numpy.typing import NDArray
 
 
+# Compute energy: (x Q x^T) given matrix Q and vector x
 def energy(Q: NDArray[np.float_], x: NDArray[np.bool_]) -> float:
-    """
-    Compute energy: (x Q x^T) given matrix Q and vector x
-
-    Args:
-        Q (n x n np.array): quadratic matrix
-        x (1 x n np.array): binary vector
-
-    Returns:
-        float: energy
-    """
     return np.matmul(np.matmul(x, Q), np.transpose(x))
 
 
-def boltzmann_factor(e_cur: float, e_can: float, beta: float) -> float:
-    """
-    Compute Boltzmann factor given a current energy state and
-    candidate energy state
-
-    Args:
-        e_cur: current state energy
-        e_can: candidate state energy
-        beta: 1 / temperature
-
-    Returns:
-        float: Boltzmann factor
-    """
-    return np.exp((e_can - e_cur) / (-beta))
+# Compute Boltzmann factor given a current state and candidate state
+def boltzmann_factor(cur_e: float, can_e: float, beta: float) -> float:
+    return np.exp((can_e - cur_e) / (-beta))
 
 
-def accept_sol(e_cur: float, e_can: float, beta: float) -> bool:
-    """
-    Metropolis-Hasting rule for accepting candidate state
-
-    Args:
-        e_cur: current state energy
-        e_can: candidate state energy
-        beta: 1/temperature
-
-    Returns:
-        bool: Truth value of statement 'candidate state is accepted'
-    """
-    if e_can <= e_cur:  # if new state has lower energy
+# Metropolis-Hasting rule for accepting candidate state
+# Return truth value of statement 'candidate state is accepted'
+def accept_sol(cur_e: float, can_e: float, beta: float) -> bool:
+    if can_e <= cur_e:  # if new state has lower energy
         return True
 
     # otherwise, accept state with probability equal to Boltzmann factor
-    return np.random.random() < boltzmann_factor(e_cur, e_can, beta)
+    return np.random.random() < boltzmann_factor(cur_e, can_e, beta)
 
 
+# Check inputs to simulated anneal.
 def input_check(
     Q: NDArray[np.float_],
     num_res: int,
     num_iters: int,
     beta_sched: NDArray[np.float_],
 ) -> None:
-    """
-    Check inputs to simulated anneal.
-
-    Args:
-        Q (n x n np.array): quadratic matrix
-        num_res (int): number of restarts in simulation
-        num_iters (int): number of iterations per restart in simulation
-        beta_sched (1 x n np.array): 1/temperature schedule
-
-    """
     # Q should be a square 2-dim matrix
     if (Q.ndim != 2) or (Q.shape[0] != Q.shape[1]):
         raise ValueError("Q is not square matrix")
@@ -100,7 +62,7 @@ def qubo_solve(
         beta_sched (1 x n np.array): 1/temperature schedule
 
     Returns:
-        NDArray[np.bool_] (1 x n np.array): minimum energy state
+        NDArray[np.bool_] (1 x n np.array): minimum energy state found
     """
 
     input_check(Q, num_res, num_iters, beta_sched)  # sanity check
@@ -112,22 +74,22 @@ def qubo_solve(
 
     for _ in range(num_res):  # given the desired # of resarts
         x = np.random.random(n) < 0.5  # initial solution is random n-bit array
-        e_cur = energy(Q, x)  # record energy of current state
+        cur_e = energy(Q, x)  # record energy of current state
 
         for i in range(num_iters):  # for each desired # of iteration
             beta = beta_sched[i]  # beta is fixed for i
 
             for j in range(n):  # for each node in solution
                 x[j] = not x[j]  # flip spin of node
-                e_can = energy(Q, x)  # record candidate energy state
+                can_e = energy(Q, x)  # record candidate energy state
 
-                if accept_sol(e_cur, e_can, beta):  # accept candidate state
-                    e_cur = e_can  # change reference energy state
+                if accept_sol(cur_e, can_e, beta):  # accept candidate state
+                    cur_e = can_e  # change reference energy state
                 else:  # reject new state
                     x[j] = not x[j]  # undo flip to spin of node
 
-        if e_cur < min_energy:  # record new minimum energy state solution
-            min_energy = e_cur
+        if cur_e < min_energy:  # record new minimum energy state solution
+            min_energy = cur_e
             best_sol = np.copy(x)
 
     return best_sol
